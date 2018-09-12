e3eb5d2345dc2e8825694d316119b586091739af
dayang <370480765@qq.com>
2018-09-12 22:56:55 +0800

    springBoot github 第二次上传

---
+++ src/test/java/com/yang/beetl/BeetlApplicationTests.java
@@ -0,0 +1,14 @@
+package com.yang.beetl;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+public class BeetlApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
